037f7b73ef61958fe641d1689626c98dfae51099
franal <pythonfrenal@gmail.com>
2026-09-22 09:47:50 +0530

    calculator

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from my_package import calculator,greeter
+print(greeter.say_hello("Rahul"))
+
+added=calculator.add(100,200)
+print("Added:",added)
+
+sub=calculator.subtract(added,20)
+print("Sub:",sub)
---
+++ my_package/greeter.py
@@ -0,0 +1,6 @@
+def say_hello(name):
+    return f"Hello My Friend,{name}!"
+
+
+
+
